9e8f9f2f5233754f466fa99cb81a2b3b226369cf
syaikhularif97-ui <syaikhularif97@gmail.com>
2026-05-11 08:53:25 +0700

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,32 @@
+const cacheName = "waterpass";
+const assets = [
+  "index.html",
+  "manifest.json",
+  "sw.js",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});sw
